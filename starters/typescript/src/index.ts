6a2352eab0c76f74bc3c855bdf1920e3c5616a6b
Warren Bickley <warren@eject.tech>
2023-04-04 17:53:53 +0100

    removed unnecessary func wrap

--- starters/typescript/src/index.ts
+++ starters/typescript/src/index.ts
@@ -2,20 +2,15 @@ import path from "path";
 import * as eject from "@eject/fastify";
 import { api } from "./api.js";
 
-// Build an API
-const start = async () => {
-  // Register Eject routes plugin
-  await api.register(eject.routes, {
-    // prefix: "/api",
-    dir: path.join(__dirname, "routes"),
-  });
+// Register Eject routes plugin
+await api.register(eject.routes, {
+  // prefix: "/api",
+  dir: path.join(__dirname, "routes"),
+});
 
-  try {
-    await api.listen({ port: parseInt(process.env.PORT || "3000") });
-  } catch (err) {
-    api.log.error(err);
-    process.exit(1);
-  }
-};
-
-start();
+try {
+  await api.listen({ port: parseInt(process.env.PORT || "3000") });
+} catch (err) {
+  api.log.error(err);
+  process.exit(1);
+}
